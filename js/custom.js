2299d9cc287bda058aaeaf2007c5ced8c2763e2e
Jonathan Ceresa <hotstepahh@hotmail.com>
2017-09-12 17:08:53 -0400

    Add alicebahlke.com to the portfolio list. Add images for alicebahlke.com. Add handler for processing the contact form.

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+
+// MODAL SECTION
 $('#portfolioModal').on('show.bs.modal', function (event) {
   var target = $(event.relatedTarget) // Button that triggered the modal
   // Extract info from data-* attributes
@@ -14,4 +16,33 @@ $('#portfolioModal').on('show.bs.modal', function (event) {
   modal.find('#portfolio-desc2').text(desc2)
   modal.find('#portfolio-url').text(url).attr("href", url)
   modal.find('#modal-image').attr("src", image)
+})
+
+// CONTACT FORM HANDLER
+var URL = 'https://pvrvxrut92.execute-api.us-east-1.amazonaws.com/Dev/contact'
+
+$('#contact-form').submit(function (event) {
+  event.preventDefault()
+
+  var data = {
+    name: $('#name').val(),
+    email: $('#email').val(),
+    description: $('#question').val()
+  }
+
+  $.ajax({
+    type: 'POST',
+    url: URL,
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+    success: function () {
+      $("#success-alert").removeClass("hidden");
+      $("#error-alert").addClass("hidden");
+    },
+    error: function () {
+      $("#success-alert").addClass("hidden");
+      $("#error-alert").removeClass("hidden");
+    }
+  })
 })
