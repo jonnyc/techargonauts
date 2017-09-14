4c443d09311ecc8315e12e819692fa47da740d31
Jonathan Ceresa <hotstepahh@hotmail.com>
2017-09-14 17:06:57 -0400

    Add Logo to the Navbar Add Media Query functionality for Logo for different resolutions Fix Typo

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,25 @@
+(function($) {
+    "use strict"; // Start of use strict
 
 // MODAL SECTION
 $('#portfolioModal').on('show.bs.modal', function (event) {
   var target = $(event.relatedTarget) // Button that triggered the modal
   // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var title = target.data('portfolioname')
   var desc1 = target.data('desc1')
   var desc2 = target.data('desc2')
   var url   = target.data('portfoliourl')
   var image = target.data('image')
   var modal = $(this)
+  // Apply Data to elements
   modal.find('.modal-title').text(title)
   modal.find('#portfolio-desc1').text(desc1)
   modal.find('#portfolio-desc2').text(desc2)
   modal.find('#portfolio-url').attr("href", url)
   modal.find('#modal-image').attr("src", image)
-})
+});
 
-// CONTACT FORM HANDLER
+// CONTACT FORM HANDLER SECTION
 var URL = 'https://pvrvxrut92.execute-api.us-east-1.amazonaws.com/Dev/contact'
 
 $('#contact-form').submit(function (event) {
@@ -45,4 +46,6 @@ $('#contact-form').submit(function (event) {
       $("#error-alert").removeClass("hidden");
     }
   })
-})
+});
+
+})(jQuery); // End of use strict
